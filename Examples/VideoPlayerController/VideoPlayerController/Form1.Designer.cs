dbe5e8a52adfc7dbf368a2bc5115059db3f94056
Sverrir Sigmundarson <sverrirs@gmail.com>
2015-11-24 22:28:16 +0100

    Adding Example VideoPlayerController
    Example solution that shows how the Xbox 360 big button controller can
    be used as a remote control for both the VLC media player and Netflix

---
+++ Examples/VideoPlayerController/VideoPlayerController/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace VideoPlayerController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbUsingVLCMediaPlayer = new System.Windows.Forms.CheckBox();
+            this.cbUsingNetflix = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbUsingVLCMediaPlayer
+            // 
+            this.cbUsingVLCMediaPlayer.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cbUsingVLCMediaPlayer.Checked = true;
+            this.cbUsingVLCMediaPlayer.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbUsingVLCMediaPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbUsingVLCMediaPlayer.Location = new System.Drawing.Point(12, 12);
+            this.cbUsingVLCMediaPlayer.Name = "cbUsingVLCMediaPlayer";
+            this.cbUsingVLCMediaPlayer.Size = new System.Drawing.Size(241, 63);
+            this.cbUsingVLCMediaPlayer.TabIndex = 0;
+            this.cbUsingVLCMediaPlayer.Text = "VLC Media Player";
+            this.cbUsingVLCMediaPlayer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cbUsingVLCMediaPlayer.UseVisualStyleBackColor = true;
+            this.cbUsingVLCMediaPlayer.CheckedChanged += new System.EventHandler(this.cbUsingVLCMediaPlayer_CheckedChanged);
+            // 
+            // cbUsingNetflix
+            // 
+            this.cbUsingNetflix.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cbUsingNetflix.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbUsingNetflix.Location = new System.Drawing.Point(12, 90);
+            this.cbUsingNetflix.Name = "cbUsingNetflix";
+            this.cbUsingNetflix.Size = new System.Drawing.Size(241, 63);
+            this.cbUsingNetflix.TabIndex = 0;
+            this.cbUsingNetflix.Text = "Netflix";
+            this.cbUsingNetflix.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cbUsingNetflix.UseVisualStyleBackColor = true;
+            this.cbUsingNetflix.CheckedChanged += new System.EventHandler(this.cbUsingNetflix_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(260, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 62);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 168);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cbUsingNetflix);
+            this.Controls.Add(this.cbUsingVLCMediaPlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbUsingVLCMediaPlayer;
+        private System.Windows.Forms.CheckBox cbUsingNetflix;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Examples/VideoPlayerController/VideoPlayerController/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XboxBigButton;
+
+namespace VideoPlayerController
+{
+    public partial class Form1 : Form
+    {
+        public enum Players
+        {
+            None,
+            VLC,
+            Netflix
+        }
+
+        /// <summary>
+        /// Brings the thread that created the specified window into the foreground and activates the window. 
+        /// Keyboard input is directed to the window.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window that should be activated and brought to the foreground.</param>
+        /// <returns>If the window was brought to the foreground, the return value is true.
+        /// If the window was not brought to the foreground, the return value is false.</returns>
+        [DllImport("user32.DLL")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        /// <summary>
+        /// Determines whether the specified window handle identifies an existing window.
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsWindow(IntPtr hWnd);
+
+        /// <summary>
+        /// The big button controller
+        /// </summary>
+        private readonly XboxBigButtonDevice _device;
+
+        /// <summary>
+        /// Time when the last keypress was received from the controller, use this to discard excessive repeating keys
+        /// </summary>
+        private DateTime _lastKeyTime = DateTime.MinValue;
+
+        /// <summary>
+        /// Handle to the VLC window
+        /// </summary>
+        private IntPtr _windowHandle = IntPtr.Zero;
+
+        /// <summary>
+        /// Which player is selected in the UI
+        /// </summary>
+        private Players _player;
+        
+        /// <summary>
+        /// Gets or sets which video player the remote controls control
+        /// </summary>
+        public Players Player
+        {
+            get { return _player; }
+            set
+            {
+                // If setting to the same thing, just exit
+                if (_player == value)
+                    return;
+
+                // Switch player
+                _player = value;
+
+                // Force a refetching of the window
+                _windowHandle = IntPtr.Zero;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _device = new XboxBigButtonDevice();
+            _device.ButtonStateChanged += _device_ButtonStateChanged;
+
+            _device.Connect();
+        }
+
+        private void _device_ButtonStateChanged(object sender, XboxBigButtonDeviceEventArgs e)
+        {
+            if (this.InvokeRequired)
+                this.BeginInvoke(new Action(() => this.SendKeysToWindow(e.Controller, e.ButtonState)));
+            else
+                this.SendKeysToWindow(e.Controller, e.ButtonState);
+        }
+
+        private void SendKeysToWindow(Controller controller, Buttons buttons)
+        {
+            // If the last key event was within tolerance then don't listen
+            if ((DateTime.Now - _lastKeyTime).TotalMilliseconds < 500)
+                return;
+
+            // The back button controls which player we're dealing with
+            if (buttons.IsPressed(Buttons.Back))
+            {
+                if (cbUsingVLCMediaPlayer.Checked)
+                    cbUsingNetflix.Checked = true;
+                else
+                    cbUsingVLCMediaPlayer.Checked = true;
+
+                // Save the time if we successfully sent keys
+                _lastKeyTime = DateTime.Now;
+
+                return;
+            }
+
+            string keysToSend;
+
+            // Which player are we dealing with
+            if (this.Player == Players.VLC)
+                keysToSend = ControlVLCPlayer(controller, buttons);
+            else if (this.Player == Players.Netflix)
+                keysToSend = ControlNetflix(controller, buttons);
+            else
+                return;
+
+            // If no keys then don't do anything
+            if (string.IsNullOrEmpty(keysToSend))
+                return;
+
+            // Set the window to foreground, only send keys if the window was successfully brought forward
+            if (SetForegroundWindow(_windowHandle))
+            {
+                // Send the keys
+                SendKeys.SendWait(keysToSend);
+
+                // Save the time if we successfully sent keys
+                _lastKeyTime = DateTime.Now;
+            }
+        }
+
+        private string ControlNetflix(Controller controller, Buttons buttons)
+        {
+            // If we don't have the VLC window yet or the window has been restarted (has a new handle)
+            // then let's try to find the handle
+            if (!IsWindowHandleValid(_windowHandle))
+            {
+                _windowHandle = FindWindowHandle("Netflix - ");
+
+                // No valid Netflix browser windowhandle could be found, exit
+                if (!IsWindowHandleValid(_windowHandle))
+                    return null;
+            }
+
+            // Translate key mappings
+            return GetNetflixKeysToSend(controller, buttons);
+        }
+
+        private string ControlVLCPlayer(Controller controller, Buttons buttons)
+        {
+            // If we don't have the VLC window yet or the window has been restarted (has a new handle)
+            // then let's try to find the handle
+            if (!IsWindowHandleValid(_windowHandle))
+            {
+                _windowHandle = FindWindowHandle("VLC media player");
+
+                // No valid VLC windowhandle could be found, exit
+                if (!IsWindowHandleValid(_windowHandle))
+                    return null;
+            }
+
+            // Translate key mappings
+            return GetVLCKeysToSend(controller, buttons);
+        }
+
+        private string GetNetflixKeysToSend(Controller controller, Buttons buttons)
+        {
+            string keys = "";
+
+            // For more info on SendKeys see:
+            // https://msdn.microsoft.com/en-us/library/system.windows.forms.sendkeys(v=vs.110).aspx
+
+            // Play/pause
+            if (buttons.IsPressed(Buttons.BigButton))
+                keys += " ";
+
+            // Skip medium forward
+            if (buttons.IsPressed(Buttons.Left))
+                keys += "{LEFT}";
+
+            // Skip medium backward
+            if (buttons.IsPressed(Buttons.Right))
+                keys += "{RIGHT}";
+
+            // Fullscreen
+            if (buttons.IsPressed(Buttons.Home))
+                keys += "f";
+
+            // Mute
+            if (buttons.IsPressed(Buttons.Y))
+                keys += "m";
+
+            return keys;
+        }
+
+        /// <summary>
+        /// Translates the big button controller keys into VLC keys
+        /// </summary>
+        private string GetVLCKeysToSend(Controller controller, Buttons buttons)
+        {
+            string keys = "";
+
+            // For more info on SendKeys see:
+            // https://msdn.microsoft.com/en-us/library/system.windows.forms.sendkeys(v=vs.110).aspx
+            
+            // Play/pause
+            if (buttons.IsPressed(Buttons.BigButton))
+                keys += " ";
+
+            // Skip medium forward
+            if (buttons.IsPressed(Buttons.Left))
+                keys += "^({LEFT})";
+
+            // Skip medium backward
+            if (buttons.IsPressed(Buttons.Right))
+                keys += "^({RIGHT})";
+
+            // Volume Up
+            if (buttons.IsPressed(Buttons.Up))
+                keys += "^({UP})";
+
+            // Volume Down
+            if (buttons.IsPressed(Buttons.Down))
+                keys += "^({DOWN})";
+
+            // Subtitles 
+            if (buttons.IsPressed(Buttons.A))
+                keys += "v";
+
+            // Audio
+            if (buttons.IsPressed(Buttons.B))
+                keys += "b";
+
+            // Fullscreen
+            if (buttons.IsPressed(Buttons.Home))
+                keys += "f";
+
+            // Mute
+            if (buttons.IsPressed(Buttons.Y))
+                keys += "m";
+
+            return keys;
+        }
+
+        private IntPtr FindWindowHandle(string windowTitle)
+        {
+            foreach (Process proc in Process.GetProcesses())
+            {
+                if (proc.MainWindowTitle.Contains(windowTitle))
+                {
+                    return proc.MainWindowHandle;
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        private bool IsWindowHandleValid(IntPtr windowHandle)
+        {
+            return windowHandle != IntPtr.Zero && IsWindow(windowHandle);
+        }
+
+
+        private void cbUsingVLCMediaPlayer_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!cbUsingVLCMediaPlayer.Checked)
+                return;
+
+            cbUsingNetflix.Checked = false;
+            this.Player = Players.VLC;
+        }
+
+        private void cbUsingNetflix_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!cbUsingNetflix.Checked) 
+                return;
+
+            cbUsingVLCMediaPlayer.Checked = false;
+            this.Player = Players.Netflix;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(_device != null )
+            {
+                _device.Disconnect();
+                _device.Dispose();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initially select the VLC button
+            cbUsingVLCMediaPlayer.Checked = true;
+        }
+    }
+
+    public static class ButtonsExtensions
+    {
+        /// <summary>
+        /// Checks if the supplied check buttons are currently pressed (note that other buttons can be pressed)
+        /// </summary>
+        /// <param name="currentState"></param>
+        /// <param name="check"></param>
+        /// <returns></returns>
+        public static bool IsPressed(this Buttons currentState, Buttons check)
+        {
+            return (currentState & check) == check;
+        }
+    }
+}
